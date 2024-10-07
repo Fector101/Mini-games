fad5d4bd6015b45b4737e4dd2d99a8eecf07a6e3
Fector101 <fabianjoseph063@gmail.com>
2024-10-07 19:35:02 +0100

    some structure

--- tetris/src/App.js
+++ tetris/src/App.js
@@ -1,14 +1,47 @@
 import logo from './logo.svg';
-import './App.css';
+import './components/css/App.css';
 import './components/css/controls.css'
+/**
+ * 
+ */
+function returnClass(str_or_list){
+  if(!str_or_list){return ''}
+  else if (typeof str_or_list === 'string'){
+    return ' '+str_or_list
+  }
+  else if (Array.isArray(str_or_list)){
+    return ' '+ str_or_list.join(' ')
+  }
+}
+function MyBtn({size,classes,text}){
+
+  return (
+          <div class={'gen-btn'+returnClass(classes)}>
+              <div></div>
+              <p>{text}</p>
+          </div>
+        ) 
+}
 function App() {
+  const setting_btns=[['start','pause'],'sound','setting',['exit','game']]
   return (
     <div className="App">
-      <div className='controls'>
-        <button className='up'></button>
-        <button className='right'></button>
-        <button className='down'></button>
-        <button className='left'></button>
+
+      <div className='controls-case'>
+
+        <div clasName="dir-case">
+          <div className="first"><MyBtn text='up / level'/></div>
+          <div className="second"><MyBtn text={['left','prev game']} size='mid'/><MyBtn text={['right','next game']} size='mid'/></div>
+          <div className="third"><MyBtn text="down / speed" size='mid'/></div>
+        </div>
+
+        <div className='settings-btns-rotate-btn-case'>
+          <div className='settings-case'>
+            {setting_btns.map(each=> <MyBtn size='small' text={each}/>)}
+          </div>
+          <MyBtn text={['rotate','direction']} size='big'/>
+        </div>
+        
       </div>
     </div>
   );
