f6d8c2adf436383664b06f88806c611572550e7c
Fector101 <fabianjoseph063@gmail.com>
2024-10-22 22:03:50 +0100

    Added Some Structure

--- tetris/src/App.js
+++ tetris/src/App.js
@@ -8,7 +8,7 @@ import "./components/css/responsive.css";
 import { randBlockName, Block} from "./components/js/blocks_construct.js";
 import { ControlsCase } from "./components/js/ControlCase.js";
 import { GameScreen } from "./components/js/GameScreen.js";
-
+import { inBounds } from "./components/js/helper.js";
 
 function App() {
   let [x, setX] = useState('3px');
@@ -52,125 +52,7 @@ function App() {
     }
     isEqualToWidth()
   }
-  function checkCollision() {
-    const container__ = document.querySelector(".screen .game").getBoundingClientRect();
-    const current_block = Array.from(document.querySelectorAll(".game .block")).at(-1).getBoundingClientRect()
-
-    if (container__.x === current_block.x) {
-      console.log("Foul");
-    }
-    // console.log('case ' + container__.x, 'block '+block.x)
-  }
-  /**
-   *  Creates App Generic Button
-   * @param {string} coord - Can be 'x' or 'y'
-   * @param {Number} pixels_to_move - The current block position i.e 'top' and 'left' neg or pos, positive if going right and vice verse
-   * @returns {Boolean} a boolean that blah blah
-   */
-  function inBounds(coord = "", pixels_to_move = 0) {
-    // For dynamic screen size
-    const container__ = document.querySelector(".screen .game").getBoundingClientRect();
-    const current_block = Array.from(document.querySelectorAll(".game .block")).at(-1)
-    const cur_block_bounds = current_block.getBoundingClientRect()
-    let state = true
-    function inScreen(){
-      if ( coord === "x" && (cur_block_bounds.x + pixels_to_move < container__.x || cur_block_bounds.x + pixels_to_move > container__.right - cur_block_bounds.width)
-      ) {
-        document.querySelector(".cur-score").textContent = "foul";
-        return false;
-      } else if ( coord === "y" && (cur_block_bounds.y + pixels_to_move < container__.y || cur_block_bounds.bottom + pixels_to_move > container__.bottom)) {
-        document.querySelector(".cur-score").textContent = "foul";
-        return false;
-      }
-      
-      // console.log('--------------------')
-      document.querySelector(".cur-score").textContent = "pass";
-      return true;
-    }
-    
-    function notCollidingWithAnotherBlock(){
-      const all_blocks = Array.from(document.querySelectorAll('.block')) // Except Current Block.
-      if(all_blocks.length === 1 )return true
-      all_blocks.pop()  // Removing element about to be checked
-      const all_blocks_Top =  all_blocks.map(e=>parseFloat(e.style.top))
-      // const all_blocks_Top =  all_blocks.map(e=>e.getBoundingClientRect().top)
-      // let A1 = all_blocks.map(e=> parseFloat(e.style.left))
-      // let A2 = all_blocks.map(e=> e.getBoundingClientRect().width + parseFloat(e.style.left) + 2 ) // 2px is the margin
-      // Checking a range of values for tyhe right side
-      const min_range = parseFloat(current_block.style.left)
-      const max_range = parseFloat(current_block.style.left) + cur_block_bounds.width 
-      const collidingLeft = B1 => B1 >= min_range && B1 <= max_range
-      const collidingRight = A1 => A1 >= min_range && A1 <= max_range
-      const collidingMiddle = (A1,B1) => A1 < min_range && B1 >= max_range
-      let blocks_under=[]
-      /**
-       * @returns {Element}
-       */
-      function blockRightUnder(){
-        let block_right_under=blocks_under[0]
-        // console.log(blocks_under)
-        blocks_under.forEach((e,i,list)=>{
-          // console.log(e)
-          if(parseFloat(block_right_under.style.top) > parseFloat(e.style.top)){
-            block_right_under=e
-          }
-          if(cur_block_bounds.top >200){
-            if(i===list.length -1){
-              console.log(block_right_under)
-            }
-          }
-        })
-        return block_right_under
-      }
-      let willCollide=false
-      // all_blocks.forEach((each_block,i)=>{
-      for (let index = 0; index < all_blocks.length; index++) {
-        const each_block = all_blocks[index]
-        const a1=parseFloat(each_block.style.left)
-        const b1=each_block.getBoundingClientRect().width + parseFloat(each_block.style.left)
-        
-        willCollide = collidingLeft(b1) || collidingRight(a1) || collidingMiddle(a1,b1)
-        // console.log(collidingLeft(b1), '||', collidingRight(a1))
-        if(willCollide){
-          // console.log(each_block)
-          blocks_under.push(each_block)
-        }
-        if(index===all_blocks.length - 1){
-          willCollide = Boolean(blocks_under.length)
-        }
-        // console.log(parseFloat(each_block.style.left) ,'>=', min_range ,'---', each_block.getBoundingClientRect().width + 2 ,'<=',max_range)
-          // if(parseFloat(each_block.style.left) >= min_range  && each_block.getBoundingClientRect().width + A1[i] <=max_range){
-          // console.log('values',parseFloat(each_block.style.left),each_block.getBoundingClientRect().width)
-          // console.log(min_range,max_range)
-          // console.log('the block',each_block)
-        
-      }
-
-
-
-      if ( coord === "x" && (cur_block_bounds.x + pixels_to_move < container__.x || cur_block_bounds.x + pixels_to_move > container__.right - cur_block_bounds.width)
-      ) {
-        document.querySelector(".cur-score").textContent = "foul";
-        return false;
-      } else if (willCollide) {// This Means it right Under
-      // } else if ( coord === "y" &&  (A1.some(e=> e >= min_range)  && A2.some(e=> e<=max_range))) {// This Means it right Under
-        console.log('Block Under.')
-        const cur_btm=parseFloat(current_block.style.top) + cur_block_bounds.height + pixels_to_move +2
-        console.log(cur_btm > blockRightUnder().style.top,cur_btm, blockRightUnder().style.top)
-        if(cur_btm > parseFloat(blockRightUnder().style.top)){  // Meet Tall Block
-          console.log(blockRightUnder())
-          return false;
-        }
-        return true
-      }
-      
-      // console.log('--------------------')
-      document.querySelector(".cur-score").textContent = "pass";
-      return true;
-    }
-    state = notCollidingWithAnotherBlock() && inScreen()
-    return state
-  }
+  
   function ANOTHER_(){
     // const parent = document.querySelector('.screen .game')
     setY('2.5px')
@@ -191,8 +73,6 @@ function App() {
     // So No lag if function becomes larger
     if (!["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(e.key))
       return;
-    // setTimeout(checkCollision,100)
-    checkCollision();
     const px = 17;
     if (["ArrowUp", "ArrowDown"].includes(e.key)) {
       setY((oldY) => {
--- tetris/src/components/js/helper.js
+++ tetris/src/components/js/helper.js
@@ -1,3 +1,4 @@
+// A collection of Helpful Independent Functions
 import {nanoid} from 'nanoid'
 /**
  *  Computes list to give string with space at front if str_or_list is not undefined
@@ -48,4 +49,97 @@ export function randInt(start=0,end){
 
 export function isTouchDevice() {
   return "ontouchstart" in window || navigator.maxTouchPoints > 0;
+}
+
+/**
+   *  Creates App Generic Button
+   * @param {string} coord - Can be 'x' or 'y'
+   * @param {Number} pixels_to_move - The current block position i.e 'top' and 'left' neg or pos, positive if going right and vice verse
+   * @returns {Boolean} a boolean that blah blah
+   */
+export function inBounds(coord = "", pixels_to_move = 0) {
+  // For dynamic screen size
+  const container__ = document.querySelector(".screen .game").getBoundingClientRect();
+  const current_block = Array.from(document.querySelectorAll(".game .block")).at(-1)
+  const cur_block_bounds = current_block.getBoundingClientRect()
+  let state = true
+  function inScreen(){
+    if ( coord === "x" && (cur_block_bounds.x + pixels_to_move < container__.x || cur_block_bounds.x + pixels_to_move > container__.right - cur_block_bounds.width)
+    ) {
+      document.querySelector(".cur-score").textContent = "foul";
+      return false;
+    } else if ( coord === "y" && (cur_block_bounds.y + pixels_to_move < container__.y || cur_block_bounds.bottom + pixels_to_move > container__.bottom)) {
+      document.querySelector(".cur-score").textContent = "foul";
+      return false;
+    }
+    
+    // console.log('--------------------')
+    document.querySelector(".cur-score").textContent = "pass";
+    return true;
+  }
+  
+  function notCollidingWithAnotherBlock(){
+    const all_blocks = Array.from(document.querySelectorAll('.block')) // Except Current Block.
+    if(all_blocks.length === 1 )return true
+    all_blocks.pop()  // Removing element about to be checked
+    // Checking a range of values for tyhe right side
+    const min_range = parseFloat(current_block.style.left)
+    const max_range = parseFloat(current_block.style.left) + cur_block_bounds.width 
+    const collidingLeft = B1 => B1 >= min_range && B1 <= max_range
+    const collidingRight = A1 => A1 >= min_range && A1 <= max_range
+    const collidingMiddle = (A1,B1) => A1 < min_range && B1 >= max_range
+    let blocks_under=[]
+    /**
+     * @returns {Element}
+     */
+    function blockRightUnder(){
+      let block_right_under=blocks_under[0]
+      blocks_under.forEach((e,i,list)=>{
+        if(parseFloat(block_right_under.style.top) > parseFloat(e.style.top)){
+          block_right_under=e
+        }
+        if(cur_block_bounds.top >200){
+          if(i===list.length -1){
+            console.log(block_right_under)
+          }
+        }
+      })
+      return block_right_under
+    }
+    let willCollide=false
+    // all_blocks.forEach((each_block,i)=>{
+    for (let index = 0; index < all_blocks.length; index++) {
+      const each_block = all_blocks[index]
+      const a1=parseFloat(each_block.style.left)
+      const b1=each_block.getBoundingClientRect().width + parseFloat(each_block.style.left)
+      
+      willCollide = collidingLeft(b1) || collidingRight(a1) || collidingMiddle(a1,b1)
+      if(willCollide){
+        blocks_under.push(each_block)
+      }
+      if(index===all_blocks.length - 1){
+        willCollide = Boolean(blocks_under.length)
+      }        
+    }
+
+
+
+    if ( coord === "x" && (cur_block_bounds.x + pixels_to_move < container__.x || cur_block_bounds.x + pixels_to_move > container__.right - cur_block_bounds.width) ) {
+      document.querySelector(".cur-score").textContent = "foul";
+      return false;
+    } else if (willCollide) {// This Means it right Under
+      const cur_btm=parseFloat(current_block.style.top) + cur_block_bounds.height + pixels_to_move +2
+      console.log(cur_btm > blockRightUnder().style.top,cur_btm, blockRightUnder().style.top)
+      if(cur_btm > parseFloat(blockRightUnder().style.top)){  // Meet Tall Block
+        return false;
+      }
+      return true
+    }
+    
+    // console.log('--------------------')
+    document.querySelector(".cur-score").textContent = "pass";
+    return true;
+  }
+  state = notCollidingWithAnotherBlock() && inScreen()
+  return state
 }
